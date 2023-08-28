1bae0c59b2e135fff5a03b8f27764dd01ccb27be
haf <712020:41b13e0c-1888-4491-9987-13bc62588a0a@bots.bitbucket.org>
2023-08-28 14:01:17 +0000

    update dashbord routes

--- src/services/dashboard/func_utils.py
+++ src/services/dashboard/func_utils.py
@@ -10,69 +10,155 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-def collection_data():
+class Facebook:
 
-    facebook_data = []
-    instagram_data = []
-    projects = Project.all(current_user)
+    def __init__(self, current_user):
+        self.current_user = current_user
+        self.data = []
 
-    for project in projects:
+    def facebook_data(self):
+        projects = Project.all(self.current_user)
 
-        if not project:
-            continue
+        for project in projects:
+            if not project:
+                continue
 
-        instagram_items = instagram.instagram_search_keyword(project.name)
+            facebook_items = facebook.facebook_search_keyword(project.name)
+            if facebook_items and 'data' in facebook_items and 'items' in facebook_items['data']:
+                self.data.extend(facebook_items['data']['items'])
+
+    def get_data(self):
+        self.facebook_data()
+        return self.data
+
+    def get_detail(self, project_name):
+        facebook_data = []
+        facebook_items = facebook.facebook_search_keyword(project_name)
+        if facebook_items and 'data' in facebook_items and 'items' in facebook_items['data']:
+            facebook_data.extend(facebook_items['data']['items'])
+        return facebook_data
+
+    def get_statistic(self, project_name):
+        data = self.get_detail(project_name)
+        shares_counts = [d.get('shares_count', 0) for d in data]
+        total_shares = sum(count for count in shares_counts if count is not None)
+
+        comments_counts = [d.get('comments_count', 0) for d in data]
+        total_comments = sum(count for count in comments_counts if count is not None)
+
+        reactions_counts = [d.get('reactions_total_count', 0) for d in data]
+        total_reactions = sum(count for count in comments_counts if count is not None)
+
+        return total_reactions, total_comments, total_shares
+
+
+class Instagram:
+
+    def __init__(self, current_user):
+        self.current_user = current_user
+        self.data = []
+
+    def collect_data(self):
+        projects = Project.all(self.current_user)
+
+        for project in projects:
+            if not project:
+                continue
+
+            instagram_items = instagram.instagram_search_keyword(project.name)
+            if instagram_items and 'data' in instagram_items and 'items' in instagram_items['data']:
+                self.data.extend(instagram_items['data']['items'])
+
+    def get_data(self):
+        self.collect_data()
+        return self.data
+
+    def get_detail(self, project_name):
+        instagram_data = []
+        instagram_items = instagram.instagram_search_keyword(project_name)
+        if instagram_items and 'data' in instagram_items and 'items' in instagram_items['data']:
+            instagram_data.extend(instagram_items['data']['items'])
+        return instagram_data
+
+    def get_statistic(self, project_name):
+        data = self.get_detail(project_name)
+        shares_counts = [d.get('shares_count', 0) for d in data]
+        total_shares = sum(count for count in shares_counts if count is not None)
+
+        comments_counts = [d.get('comments_count', 0) for d in data]
+        total_comments = sum(count for count in comments_counts if count is not None)
+
+        reactions_counts = [d.get('reactions_total_count', 0) for d in data]
+        total_reactions = sum(count for count in comments_counts if count is not None)
+
+        return total_reactions, total_comments, total_shares
+
+
+class CombinedData:
+
+    def __init__(self, current_user):
+        self.current_user = current_user
+        self.facebook_collector = Facebook(self.current_user)
+        self.instagram_collector = Instagram(self.current_user)
+
+    def get_combined_data(self):
+        facebook_data = self.facebook_collector.get_data()
+        instagram_data = self.instagram_collector.get_data()
+        return list(chain(instagram_data, facebook_data))
+
+    def get_detail(self, project_name):
+        instagram_data = []
+        facebook_data = []
+
+        instagram_items = instagram.instagram_search_keyword(project_name)
         if instagram_items and 'data' in instagram_items and 'items' in instagram_items['data']:
             instagram_data.extend(instagram_items['data']['items'])
 
-        facebook_items = facebook.facebook_search_keyword(project.name)
+        facebook_items = facebook.facebook_search_keyword(project_name)
         if facebook_items and 'data' in facebook_items and 'items' in facebook_items['data']:
             facebook_data.extend(facebook_items['data']['items'])
 
-    combined_data = list(chain(instagram_data, facebook_data))
+        combined_data = list(chain(instagram_data, facebook_data))
+        return combined_data
 
-    return combined_data
+    def get_stat_facebook(self):
+        data = self.facebook_collector.get_data()
 
+        shares_counts = [d.get('shares_count', 0) for d in data]
+        total_shares = sum(count for count in shares_counts if count is not None)
 
-def collection_statistic():
-    data = collection_data()
-    shares_counts = [d.get('shares_count', 0) for d in data]
-    total_shares = sum(count for count in shares_counts if count is not None)
+        comments_counts = [d.get('comments_count', 0) for d in data]
+        total_comments = sum(count for count in comments_counts if count is not None)
 
-    comments_counts = [d.get('comments_count', 0) for d in data]
-    total_comments = sum(count for count in comments_counts if count is not None)
+        reactions_counts = [d.get('reactions_total_count', 0) for d in data]
+        total_reactions = sum(count for count in reactions_counts if count is not None)
 
-    reactions_counts = [d.get('reactions_total_count', 0) for d in data]
-    total_reactions = sum(count for count in comments_counts if count is not None)
+        return total_reactions, total_comments, total_shares
 
-    return total_reactions, total_comments, total_shares
+    def get_stat_instagram(self):
+        data = self.instagram_collector.get_data()
 
+        shares_counts = [d.get('shares_count', 0) for d in data]
+        total_shares = sum(count for count in shares_counts if count is not None)
 
-def collection_data_detail(project_name):
-    facebook_data = []
-    instagram_data = []
-    instagram_items = instagram.instagram_search_keyword(project_name)
-    if instagram_items and 'data' in instagram_items and 'items' in instagram_items['data']:
-        instagram_data.extend(instagram_items['data']['items'])
+        comments_counts = [d.get('comments_count', 0) for d in data]
+        total_comments = sum(count for count in comments_counts if count is not None)
 
-    facebook_items = facebook.facebook_search_keyword(project_name)
-    if facebook_items and 'data' in facebook_items and 'items' in facebook_items['data']:
-        facebook_data.extend(facebook_items['data']['items'])
+        reactions_counts = [d.get('reactions_total_count', 0) for d in data]
+        total_reactions = sum(count for count in reactions_counts if count is not None)
 
-    combined_data = list(chain(instagram_data, facebook_data))
+        return total_reactions, total_comments, total_shares
 
-    return combined_data
+    def get_statistics(self):
+        data = self.get_combined_data()
 
+        shares_counts = [d.get('shares_count', 0) for d in data]
+        total_shares = sum(count for count in shares_counts if count is not None)
 
-def collection_statistic_detail(project_name):
-    data = collection_data_detail(project_name)
-    shares_counts = [d.get('shares_count', 0) for d in data]
-    total_shares = sum(count for count in shares_counts if count is not None)
+        comments_counts = [d.get('comments_count', 0) for d in data]
+        total_comments = sum(count for count in comments_counts if count is not None)
 
-    comments_counts = [d.get('comments_count', 0) for d in data]
-    total_comments = sum(count for count in comments_counts if count is not None)
+        reactions_counts = [d.get('reactions_total_count', 0) for d in data]
+        total_reactions = sum(count for count in reactions_counts if count is not None)
 
-    reactions_counts = [d.get('reactions_total_count', 0) for d in data]
-    total_reactions = sum(count for count in comments_counts if count is not None)
-
-    return total_reactions, total_comments, total_shares
+        return total_reactions, total_comments, total_shares
--- src/services/dashboard/routes.py
+++ src/services/dashboard/routes.py
@@ -8,16 +8,14 @@ from flask import url_for
 from flask_login import current_user
 from flask_login import login_required
 
-from src.exts import cache
 from src.services.account import Project
 from src.services.account import utils
 from src.services.account.forms import ProjectForm
 
 from .func_utils import(
-    collection_data,
-    collection_statistic,
-    collection_data_detail,
-    collection_statistic_detail
+    Facebook,
+    Instagram,
+    CombinedData
 )
 
 
@@ -26,23 +24,28 @@ dashboard_bp = Blueprint("dashboard_bp", __name__, url_prefix="/panel/~/")
 
 @dashboard_bp.get("/dashboard/")
 @login_required
-@cache.cached(timeout=300)
 def dashboard():
     page_title = "Tableau de bord"
-    projects = collection_data()[:10]
+    data = CombinedData(current_user)
+    items_data = data.get_combined_data()
+    stats_data = data.get_statistics()
+
+    stat_fb = data.get_stat_facebook()
+    stat_insta = data.get_stat_instagram()
 
     return render_template(
         "dashboard/_base.html",
         page_title=page_title,
         user=current_user,
-        items=projects,
-        stat=collection_statistic()
+        items=items_data,
+        stat=stats_data,
+        stat_fb=stat_fb,
+        stat_insta=stat_insta
     )
 
 
 @dashboard_bp.get("/project/")
 @login_required
-@cache.cached(timeout=500)
 def get_projects():
     page_title = "projets"
     projects = Project.all(current_user)
@@ -56,24 +59,78 @@ def get_projects():
 
 @dashboard_bp.get("/result/<string:public_id>")
 @login_required
-@cache.cached(timeout=500)
-def detail_project(public_id):
+def detail_project(public_id: str):
     project = utils.abort_if_project_doesnt_exist(public_id)
-    project_name = page_title = project.name
-    project_data = collection_data_detail(project_name)
-    project_stat = collection_statistic_detail(project_name)
+
+    page_title = project.name
+    page_title = f"Tag '{project.name}' data"
+
+    data = CombinedData(current_user)
+    items_data = data.get_detail(project.name)
+    stats_data = data.get_statistics()
+
+    stat_fb = data.get_stat_facebook()
+    stat_insta = data.get_stat_instagram()
+
 
     return render_template(
         "project/detail.html",
+        project=project,
         user=current_user,
         page_title=page_title,
-        item=project_data,
-        stat=project_stat
+        item=items_data,
+        stat=stats_data,
+        stat_fb=stat_fb,
+        stat_insta=stat_insta
     )
+
+
+@dashboard_bp.get("/result/~/fb/<string:public_id>")
+@login_required
+def facebook_detail_project(public_id: str):
+    project = utils.abort_if_project_doesnt_exist(public_id)
+
+    page_title = f"Facebook data : '{project.name}'"
+
+    data = Facebook(current_user)
+    items_data = data.get_detail(project.name)
+    stats_data = data.get_statistic(project.name)
+
+    return render_template(
+        "project/fb.html",
+        project=project,
+        user=current_user,
+        page_title=page_title,
+        item=items_data,
+        stat=stats_data
+    )
+
+
+@dashboard_bp.get("/result/~/in/<string:public_id>")
+@login_required
+def instagram_detail_project(public_id: str):
+    project = utils.abort_if_project_doesnt_exist(public_id)
+
+    page_title = f"Instagram data : '{project.name}'"
+
+    data = Instagram(current_user)
+    items_data = data.get_detail(project.name)
+    stats_data = data.get_statistic(project.name)
+
+    return render_template(
+        "project/fb.html",
+        project=project,
+        user=current_user,
+        page_title=page_title,
+        item=items_data,
+        stat=stats_data
+    )
+
+
 
 @dashboard_bp.route("/update/<string:public_id>", methods=["GET", "POST"])
 @login_required
-def update_project(public_id):
+def update_project(public_id: str):
     page_title = "Mettre à jour les mots-clés/phrases-clés"
     project = utils.abort_if_project_doesnt_exist(public_id)
 
@@ -90,7 +147,7 @@ def update_project(public_id):
 
 @dashboard_bp.post("/delete/<string:public_id>/")
 @login_required
-def delete_project(public_id):
+def delete_project(public_id: str):
     project = utils.abort_if_project_doesnt_exist(public_id)
     if project.user_id != current_user.id:
         abort(403)
