0728de8581a5006509ec947c1dbed5dabf0ca90c
haf <712020:41b13e0c-1888-4491-9987-13bc62588a0a@bots.bitbucket.org>
2023-08-28 09:42:09 +0000

    define collection data

--- src/services/dashboard/routes.py
+++ src/services/dashboard/routes.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 from flask import abort
 from flask import Blueprint
 from flask import redirect
@@ -5,26 +7,42 @@ from flask import render_template
 from flask import url_for
 from flask_login import current_user
 from flask_login import login_required
+
+from src.exts import cache
 from src.services.account import Project
 from src.services.account import utils
 from src.services.account.forms import ProjectForm
-from src.services.apis import instagram
+
+from .func_utils import(
+    collection_data,
+    collection_statistic,
+    collection_data_detail,
+    collection_statistic_detail
+)
 
 
-dashboard_bp = Blueprint("dashboard_bp", __name__, url_prefix="/panel/")
+dashboard_bp = Blueprint("dashboard_bp", __name__, url_prefix="/panel/~/")
 
 
-@dashboard_bp.get("/")
+@dashboard_bp.get("/dashboard/")
 @login_required
+@cache.cached(timeout=300)
 def dashboard():
     page_title = "Tableau de bord"
+    projects = collection_data()[:10]
+
     return render_template(
-        "dashboard/_base.html", page_title=page_title, user=current_user
+        "dashboard/_base.html",
+        page_title=page_title,
+        user=current_user,
+        items=projects,
+        stat=collection_statistic()
     )
 
 
-@dashboard_bp.get("/projects/")
+@dashboard_bp.get("/project/")
 @login_required
+@cache.cached(timeout=500)
 def get_projects():
     page_title = "projets"
     projects = Project.all(current_user)
@@ -36,28 +54,22 @@ def get_projects():
     )
 
 
-@dashboard_bp.get("/results/<string:public_id>")
+@dashboard_bp.get("/result/<string:public_id>")
 @login_required
+@cache.cached(timeout=500)
 def detail_project(public_id):
     project = utils.abort_if_project_doesnt_exist(public_id)
-    page_title = project.name
-
-    _stats = instagram.instagram_stats()
-    keyword = instagram.instagram_search_keywords(page_title)
-    _keyword_items = keyword['data']['items']
-
-    _item_type = instagram.instagram_list_created_tasks("tag")
+    project_name = page_title = project.name
+    project_data = collection_data_detail(project_name)
+    project_stat = collection_statistic_detail(project_name)
 
     return render_template(
         "project/detail.html",
         user=current_user,
         page_title=page_title,
-        project=project,
-        instagram_stats=_stats,
-        instagram_items=_item_type,
-        instagram_data=_keyword_items,
+        item=project_data,
+        stat=project_stat
     )
-
 
 @dashboard_bp.route("/update/<string:public_id>", methods=["GET", "POST"])
 @login_required
