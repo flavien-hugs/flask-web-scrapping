9519cd6a5c0bb4529a356cb570e990534e211d7c
haf <712020:41b13e0c-1888-4491-9987-13bc62588a0a@bots.bitbucket.org>
2023-12-05 20:45:36 +0000

    update config app

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo "Starting entrypoint.sh"
+
 # Initialisation de la base de données Flask
 python runserver.py flask flask db init
 
 # Initialisation de la base de données
 python runserver.py flask init_db
 
+echo "Entrypoint.sh executed successfully"
 exec "$@"
